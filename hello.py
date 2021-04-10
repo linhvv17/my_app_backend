92215994a12399377dd91d996132a38dd9e9ed74
linhvv17 <linhvv17@gmail.com>
2021-04-10 21:42:57 +0700

    make it better

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 from flask import Flask, jsonify
 
 from pyngrok import ngrok
+
+#kết nối với DATABASE_URL
+import os
+import psycopg2
+
 url = ngrok.connect(6868).public_url
 print('Henzy Tunnel URL:', url)
+
+
+#kết nối với DATABASE_URLmã của bạn.
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 app = Flask(__name__) #tao mot instance cua class flask
